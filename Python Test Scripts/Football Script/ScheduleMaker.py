416a64447442e46bcd4e09ca52783133e82a6cd5
Anthony <ant21san@gmail.com>
2021-11-19 14:18:23 -0600

    edited ScheduleMaker.py

--- Python Test Scripts/Football Script/ScheduleMaker.py
+++ Python Test Scripts/Football Script/ScheduleMaker.py
@@ -1,4 +1,3 @@
-
 def roundRobin(teams,numFix):
 	#numFix = numFix - 1 
 	if(len(teams) % 2 != 0):
@@ -9,7 +8,7 @@ def roundRobin(teams,numFix):
 	for i in range(0,int(len(teams)-numFix)):
 
 		fixed = teams[0:numFix] #fixed
-		print(fixed)
+		#print(fixed)
 		move = teams[int(numFix)]
 		rest = teams[int(numFix)+1:]
 		rest.append(move)
@@ -18,17 +17,30 @@ def roundRobin(teams,numFix):
 		print(teams)
 
 		for i in range(0,int(len(teams)-halfSize)):
-			print("team "+teams[i]+" vs "+"team "+teams[int(i+halfSize)]) #save into usable data structure
-
-
+			print(teams[i]+" vs "+teams[int(i+halfSize)]) #save into usable data structure
+			#{"Round": i,
+			# "games": [
+				#team1: opponent,
+				#team2: opponent,
+				#team3: opponent,
+				#team4: opponent,
+				#team5: opponent,
+				#team6: opponent,
+				#team7: opponent]
+			#},
 	return teams
+	#return RRdictionary
 	
-teams = ['a','b','c','d','e','f']
-longteams = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14']
-longteams = roundRobin(longteams,7)
+# teams = ['a','b','c','d','e','f']
+# longteams = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14']
+# longteams = roundRobin(longteams,7)
 
+SECWteams = ['Alabama','Texas A&M','Ole Miss','Arkansas','Miss St.','Arkansas','LSU']
+SECEteams = ['Georgia','Kentucky','Tennessee','Florida','Mizzou','South Carolina','Vandy']
+SECteams = SECWteams + SECEteams
+FBSteams = []
 
-
+roundRobin(SECWteams,1)
 #ACTUAL CODE
 
 ##Round Robins
